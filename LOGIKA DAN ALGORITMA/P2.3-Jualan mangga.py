06299032e1167182e728ec8121bc2d4e455da641
gibrandevlab <146094525+gibrandevlab@users.noreply.github.com>
2023-09-26 19:54:01 +0700

    Delete LOGIKA DAN ALGORITMA directory

--- LOGIKA DAN ALGORITMA/P2.3-Jualan mangga.py
+++
@@ -1,9 +0,0 @@
-harga_kg = int(input("Harga mangga Per KG : RP."))
-kg = float(input("Jumlah Berat Pembelian: ")) 
-
-
-total_harga = harga_kg + kg
-
-
-
-print("Total Harga yang dibayar pembeli", total_harga)
