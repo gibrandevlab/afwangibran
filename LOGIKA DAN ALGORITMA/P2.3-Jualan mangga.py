62b2f95677d9997d4ac08be7860cb0f6d76535d5
gibrandevlab <146094525+gibrandevlab@users.noreply.github.com>
2023-09-26 19:41:38 +0700

    Add files via upload

---
+++ LOGIKA DAN ALGORITMA/P2.3-Jualan mangga.py
@@ -0,0 +1,9 @@
+harga_kg = int(input("Harga mangga Per KG : RP."))
+kg = float(input("Jumlah Berat Pembelian: ")) 
+
+
+total_harga = harga_kg + kg
+
+
+
+print("Total Harga yang dibayar pembeli", total_harga)
